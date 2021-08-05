0bd88e6c177fd61790efc946a3783f1559466377
a63933 <1324147533@qq.com>
2021-08-05 20:50:33 +0800

    m

---
+++ main.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const path = require('path')
+const { Transform } = require('stream')
+
+let pathObj = {
+    source: path.resolve(process.cwd(), 'source.html'),
+    target: path.resolve(process.cwd(), 'index.html')
+}
+
+fs.writeFileSync(pathObj.source, `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>`)
+
+fs.watch(pathObj.source, () => {
+    let rs = fs.createReadStream(pathObj.source)
+
+    // rs.on('data', (chunk) => {
+    //     console.log(chunk.toString())
+    // })
+    
+    let ws = fs.createWriteStream(pathObj.target)
+    
+    let oT = new Transform({
+        transform(chunk, encoding, callback){
+            let input = chunk.toString()
+            let output = input.toUpperCase()
+            callback(null, output)
+        }
+    })
+    
+    rs.pipe(oT).pipe(ws);
+})
+
